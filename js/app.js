f73dafe96dcee4d104dedec4be3ba90795bf04f7
Kirsty <kirstmcbride@gmail.com>
2023-06-26 14:44:14 +0100

    second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+
+const form = document.querySelector('#new-item-form');
+const formHandler = (event) => {
+  event.preventDefault();
+  console.log(event.target.title.value);
+}
+form.addEventListener('submit', formHandler);
+
 })
